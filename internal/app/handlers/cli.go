86a7c3dc8dc709410bfb4978af554eaf6e07a8ad
0x0FACED <https.genius@gmail.com>
2024-10-20 07:36:18 +0300

    chore(handlers): changed return type to ptr

--- internal/app/handlers/cli.go
+++ internal/app/handlers/cli.go
@@ -1,13 +1,17 @@
 package handlers
 
-import "github.com/0x0FACED/locked/internal/app/services"
+import (
+	"context"
+
+	"github.com/0x0FACED/locked/internal/app/services"
+)
 
 type CLIHandler struct {
-	secretSrv services.SecretService
+	secretService services.SecretService
 }
 
-func NewCLI(srv services.SecretService) CLIHandler {
-	return CLIHandler{
-		secretSrv: srv,
+func NewCLI(srv services.SecretService) *CLIHandler {
+	return &CLIHandler{
+		secretService: srv,
 	}
 }
--- internal/app/handlers/web.go
+++ internal/app/handlers/web.go
@@ -1,13 +1,19 @@
 package handlers
 
-import "github.com/0x0FACED/locked/internal/app/services"
+import (
+	"net/http"
+
+	"github.com/0x0FACED/locked/internal/app/services"
+)
 
 type WebHandler struct {
+	server    http.Server
 	secretSrv services.SecretService
 }
 
-func NewWeb(srv services.SecretService) WebHandler {
-	return WebHandler{
+func NewWeb(srv services.SecretService) *WebHandler {
+	return &WebHandler{
+		server:    http.Server{}, // временно
 		secretSrv: srv,
 	}
 }
