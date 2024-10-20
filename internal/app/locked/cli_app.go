956286d828d80f747495d49ffc75c2c415df94ff
0x0FACED <https.genius@gmail.com>
2024-10-20 12:59:41 +0300

    chore(cli): updated add command code, added readline and completer

--- internal/app/locked/cli_app.go
+++ internal/app/locked/cli_app.go
@@ -1,34 +1,78 @@
 package locked
 
 import (
-	"bufio"
 	"context"
 	"errors"
+	"flag"
 	"fmt"
 	"os"
 	"strings"
 	"syscall"
 	"time"
 
-	"github.com/0x0FACED/locked/internal/app/handlers"
 	"github.com/0x0FACED/locked/internal/app/services"
+	"github.com/0x0FACED/locked/internal/core/models"
+	"github.com/chzyer/readline"
 	"golang.org/x/term"
 )
 
 type cliApp struct {
-	cliHandler  *handlers.CLIHandler
-	currentFile *os.File
+	secretService services.SecretService
+	currentFile   *os.File
 }
 
-func NewCLIApp() *cliApp {
-	secretService := services.New()
+// Реализация автодополнения команд
+type autoCompleter struct{}
+
+func (a autoCompleter) Do(line []rune, pos int) (newLine [][]rune, offset int) {
+	commands := []string{"add", "open", "close", "exit"}
+	for _, cmd := range commands {
+		if strings.HasPrefix(string(line), cmd) {
+			newLine = append(newLine, []rune(cmd))
+		}
+	}
+	return newLine, len(line)
+}
+
+func NewCLIApp(resCh chan []byte, errCh chan error, done chan struct{}) *cliApp {
+	secretService := services.New(resCh, errCh, done)
 	return &cliApp{
-		cliHandler: handlers.NewCLI(secretService),
+		secretService: secretService,
 	}
 }
 
 func (a *cliApp) StartCLI(ctx context.Context) error {
-	reader := bufio.NewReader(os.Stdin)
+	// Инициализация autocomplete с командами
+	commands := []string{"add", "open", "clear", "close", "del", "exit"}
+
+	// Создание обработчика для автодополнения
+	completer := readline.NewPrefixCompleter(
+		readline.PcItem("add"),
+		readline.PcItem("open"),
+		readline.PcItem("clear"),
+		readline.PcItem("close"),
+		readline.PcItem("del"),
+		readline.PcItem("exit"),
+	)
+
+	// Инициализируем readline
+	rl, err := readline.NewEx(&readline.Config{
+		Prompt:          "locked ~# ",
+		InterruptPrompt: "^C",
+		EOFPrompt:       "exit",
+		HistoryFile:     "/tmp/readline.tmp",
+		AutoComplete:    completer, // Поддержка автозаполнения
+	})
+	if err != nil {
+		return err
+	}
+
+	defer rl.Close()
+
+	// Каналы для обработки ввода и ошибок
+	inputCh := make(chan string)
+	errCh := make(chan error)
+
 	for {
 		err := requestPassword()
 		if err != nil {
@@ -39,27 +83,17 @@ func (a *cliApp) StartCLI(ctx context.Context) error {
 			break
 		}
 	}
+
 	fmt.Println("Welcome to the CLI Application! Type 'exit' to quit.")
 
 	for {
 		if a.currentFile != nil {
-			fmt.Printf("locked/%s ~# ", a.currentFile.Name())
+			rl.SetPrompt(fmt.Sprintf("locked/%s ~# ", a.currentFile.Name()))
 		} else {
-			fmt.Printf("locked ~# ")
+			rl.SetPrompt("locked ~# ")
 		}
 
-		inputCh := make(chan string, 1)
-		errCh := make(chan error, 1)
-
-		// читаем из консоли в отдельной горутине
-		go func() {
-			input, err := reader.ReadString('\n')
-			if err != nil {
-				errCh <- err
-			} else {
-				inputCh <- input
-			}
-		}()
+		go readCmd(rl, inputCh, errCh)
 
 		select {
 		case <-ctx.Done(): // graceful shutdown
@@ -74,7 +108,36 @@ func (a *cliApp) StartCLI(ctx context.Context) error {
 					fmt.Println("You need to open any of your secret files or create one to keep a secret.")
 					fmt.Println("To open the file, type the following command: open filename.lkd")
 				} else {
-					fmt.Println("You entered:", words[1])
+					// сет флагов для команды add
+					addCmd := flag.NewFlagSet("add", flag.ContinueOnError)
+
+					// флаги
+					// TODO: как-то это все структурировать в одном месте, а не при каждом вызове функции
+					name := addCmd.String("n", "", "Name of the secret")
+					desc := addCmd.String("d", "", "Description of the secret")
+					payload := addCmd.String("s", "", "Secret payload or file path")
+
+					// парсинг
+					if err := addCmd.Parse(words[1:]); err != nil {
+						fmt.Println("Error parsing flags:", err)
+						return nil
+					}
+
+					// проверяем, что обязательные флаги есть (они все обязательные лол)
+					if *name == "" || *desc == "" || *payload == "" {
+						fmt.Println("Usage: add -n <name> -d <description> -s <payload>")
+						return nil
+					}
+
+					// для удобства юзаем структуру параметров
+					params := models.AddSecretCmdParams{
+						Name:        *name,
+						Description: *desc,
+						Payload:     *payload, // это может быть путь к файлу или текстовый payload
+					}
+
+					// Вызываем метод для добавления секрета
+					a.secretService.Add(ctx, params)
 				}
 
 			case "open":
@@ -90,7 +153,11 @@ func (a *cliApp) StartCLI(ctx context.Context) error {
 			case "clear": // очистка всего файла с секретами
 
 			case "close": // закрыть файл, но не приложение
-
+				err := a.currentFile.Close()
+				if err != nil {
+					fmt.Println("Error closing the file:", err)
+				}
+				a.currentFile = nil
 			case "del": // удалить секрет из файла
 			case "exit": // выход из приложения
 				fmt.Println("Exiting the application. Goodbye!")
@@ -102,6 +169,16 @@ func (a *cliApp) StartCLI(ctx context.Context) error {
 			fmt.Println("Error reading input:", err)
 		}
 	}
+}
+
+func readCmd(rl *readline.Instance, inputCh chan string, errCh chan error) {
+	line, err := rl.Readline()
+	if err != nil {
+		if len(line) != 0 {
+			errCh <- err
+		}
+	}
+	inputCh <- line
 }
 
 // запрашиваем ввод пароля
