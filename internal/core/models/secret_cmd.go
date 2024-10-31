e23de3c9b57968895ac906e62358a2ac6b1d2e81
0x0FACED <https.genius@gmail.com>
2024-10-31 13:39:27 +0300

    refactor: name, desc and payload are ptrs now, added IsFile flag to add params struct

--- internal/core/models/secret_cmd.go
+++ internal/core/models/secret_cmd.go
@@ -3,7 +3,6 @@ package models
 import "github.com/0x0FACED/locked/internal/core/models/types"
 
 // Эта структура нужна для отображения секрета в консоли/web/desktop UI
-
 type SecretUI struct {
 	ID          types.SecretID
 	Name        types.SecretName
@@ -18,8 +17,8 @@ type SecretUI struct {
 // Это параметры секрета (то, что вводится в консоль)
 // А там вводиться будут только название, описание и путь до файла (или сам секрет в виде текста)
 type AddSecretCmdParams struct {
-	Name        string
-	Description string
+	Name        *string
+	Description *string
 	// Строка, ибо эта структура будет использоваться, когда пользователь вводит данные в консоль чисто.
 	// Поэтому у нас полезная нагрузка - это строка.
 	// Если это будет просто текст - значит это и есть секрет.
@@ -29,5 +28,7 @@ type AddSecretCmdParams struct {
 	// Будем через постфикс в конце слова
 	// Условно, если в конце есть .txt или .jpg, значит это ФАЙЛ.
 	// Если же будет такая запись: /path/to/file, то прога это воспримет как ТЕКСТ (в конце нет расширения файла).
-	Payload string
+	Payload *string
+
+	IsFile bool
 }
