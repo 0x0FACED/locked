405f88f6bfeab5d8f1b05b9a0671e661f99cca02
0x0FACED <https.genius@gmail.com>
2024-10-31 17:27:01 +0300

    chore(serialize): added serialization v1

--- internal/app/services/serialize.go
+++ internal/app/services/serialize.go
@@ -1,15 +1,41 @@
 package services
 
 import (
+	"bytes"
 	"context"
+	"encoding/binary"
 
 	"github.com/0x0FACED/locked/internal/core/models"
-	"github.com/0x0FACED/locked/internal/core/models/types"
 )
 
-func (s *secretService) Serialize(ctx context.Context, secret models.AddSecretCmdParams) ([]byte, error) {
-	var rec models.SecretRecord
-	off := s.db.Offset()
-	rec.Offset = types.SecretOffset(off)
-	
+func Serialize(ctx context.Context, record models.SecretRecord) ([]byte, error) {
+	buf := new(bytes.Buffer)
+
+	// offset -> id -> name -> desc -> type -> created at -> size -> pload
+	if err := binary.Write(buf, binary.LittleEndian, record.Offset); err != nil {
+		return nil, err
+	}
+	if err := binary.Write(buf, binary.LittleEndian, record.ID); err != nil {
+		return nil, err
+	}
+	if _, err := buf.Write(record.Name[:]); err != nil {
+		return nil, err
+	}
+	if _, err := buf.Write(record.Description[:]); err != nil {
+		return nil, err
+	}
+	if err := binary.Write(buf, binary.LittleEndian, record.Type); err != nil {
+		return nil, err
+	}
+	if err := binary.Write(buf, binary.LittleEndian, record.CreatedAt); err != nil {
+		return nil, err
+	}
+	if err := binary.Write(buf, binary.LittleEndian, record.Size); err != nil {
+		return nil, err
+	}
+	if _, err := buf.Write(record.Payload); err != nil {
+		return nil, err
+	}
+
+	return buf.Bytes(), nil
 }
