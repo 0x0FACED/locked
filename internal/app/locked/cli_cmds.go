3f1a06d7d39ca337f3ccb4e223ceb383ffebbf03
0x0FACED <https.genius@gmail.com>
2024-10-31 13:38:31 +0300

    chore(cli-add): updated add cmd, added secret flag divided to -p (path/to/file) and -s (secret - text)

--- internal/app/locked/cli_cmds.go
+++ internal/app/locked/cli_cmds.go
@@ -2,8 +2,10 @@ package locked
 
 import (
 	"context"
+	"errors"
 	"flag"
-	"fmt"
+
+	"github.com/0x0FACED/locked/internal/core/models"
 )
 
 // Здесь мы обрабатываем флаги и вызываем метод сервиса
@@ -15,31 +17,50 @@ func (a *cliApp) add(ctx context.Context, args []string) {
 	// то в горутине запускаем процесс, если же не фоновый, то блкокируем основной ввод и ждем.
 
 	// флаги
-	name := addCmd.String("n", "", "Name of the secret")
-	desc := addCmd.String("d", "", "Description of the secret")
-	payload := addCmd.String("s", "", "Secret payload or file path")
+	name := flag.String("n", "", "Name of the secret")
+	desc := flag.String("d", "", "Description of the secret")
+	secretText := flag.String("s", "", "Secret text data")
+	secretFile := flag.String("p", "", "Path to file containing secret")
 
 	// парсинг аргументов
 	if err := addCmd.Parse(args); err != nil {
-		fmt.Println("Error parsing flags:", err)
 		a.errCh <- err
-	}
-
-	// проверка, что все обязательные флаги заполнены
-	if *name == "" || *desc == "" || *payload == "" {
-		fmt.Println("Usage: add -n <name> -d <description> -s <payload>")
 		return
 	}
 
-	// создание параметров команды
-	//params := models.AddSecretCmdParams{
-	//	Name:        *name,
-	//	Description: *desc,
-	//	Payload:     *payload,
-	//}
+	if *secretText != "" && *secretFile != "" {
+		a.errCh <- errors.New("~ Error: Please specify only one of -s (secret text) or -p (secret file path)")
+		return
+	}
+
+	// НЕ ЗДЕСЬ
+	/*
+		// проверка, что все обязательные флаги заполнены
+		if *name == "" || *desc == "" || *payload == "" {
+			fmt.Println("Usage: add -n <name> -d <description> -s <payload>")
+			return
+		}
+	*/
+
+	// инициализация параметров для передачи в сервис
+	var params models.AddSecretCmdParams
+	params.Name = name
+	params.Description = desc
+
+	// Проверка на тип секрета (текст или файл) и установка параметров
+	if *secretText != "" {
+		params.Payload = secretText
+		params.IsFile = false
+	} else if *secretFile != "" {
+		params.Payload = secretFile
+		params.IsFile = true
+	} else {
+		a.errCh <- errors.New("~ Error: Either -s (secret text) or -p (secret file path) must be provided")
+		return
+	}
 
 	// вызов метода для добавления секрета
-	//a.secretService.Add(ctx, params)
+	a.secretService.Add(ctx, params)
 }
 
 func (a *cliApp) open(ctx context.Context, filename string) {
