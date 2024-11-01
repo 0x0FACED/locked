cce0fa4f7c93bbca4ae944d3913c1159b8c2cb35
0x0FACED <https.genius@gmail.com>
2024-11-01 15:51:36 +0300

    chore: updated encryptor, added nonce to aes struct, changed Ecnrypt method

--- internal/core/encryption/encryptor.go
+++ internal/core/encryption/encryptor.go
@@ -1,60 +1,36 @@
 package encryption
 
 import (
-	"bytes"
 	"context"
 	"crypto/aes"
 	"crypto/cipher"
-	"crypto/rand"
-	"io"
-
-	"github.com/0x0FACED/locked/internal/core/models/types"
-	"golang.org/x/crypto/scrypt"
+	"fmt"
 )
 
 type Encryptor interface {
-	Encrypt(ctx context.Context, data []types.SecretPayload, resultCh chan<- []byte, errCh chan<- error)
+	Encrypt(ctx context.Context, data []byte) ([]byte, error)
 }
 
 type aesEncryptor struct {
-	key []byte
+	key   []byte   // хэш мастер-пароля
+	nonce [12]byte // nonce, уникальный для каждого ФАЙЛА (в заголовке лежит)
 }
 
-func NewEnc(masterPassword string) (*aesEncryptor, error) {
-	salt := []byte("test_salt")
-	key, err := scrypt.Key([]byte(masterPassword), salt, 32768, 8, 1, 32)
+func NewAesEncryptor(key []byte, nonce [12]byte) *aesEncryptor {
+	return &aesEncryptor{key: key, nonce: nonce}
+}
+
+func (e *aesEncryptor) Encrypt(ctx context.Context, data []byte) ([]byte, error) {
+	block, err := aes.NewCipher(e.key)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to create AES cipher: %w", err)
 	}
-	return &aesEncryptor{key: key}, nil
-}
 
-func (e *aesEncryptor) Encrypt(ctx context.Context, data []types.SecretPayload, resultCh chan<- []byte, errCh chan<- error) {
-	for _, payload := range data {
-		iv := make([]byte, aes.BlockSize)
-		if _, err := io.ReadFull(rand.Reader, iv); err != nil {
-			errCh <- err
-			return
-		}
-
-		block, err := aes.NewCipher(e.key)
-		if err != nil {
-			errCh <- err
-			return
-		}
-
-		paddedData := pad(payload)
-
-		encrypted := make([]byte, len(paddedData))
-		mode := cipher.NewCBCEncrypter(block, iv)
-		mode.CryptBlocks(encrypted, paddedData)
-
-		resultCh <- append(iv, encrypted...)
+	aesgcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create GCM: %w", err)
 	}
-}
 
-func pad(data []byte) []byte {
-	padding := aes.BlockSize - len(data)%aes.BlockSize
-	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
-	return append(data, padtext...)
+	ciphertext := aesgcm.Seal(nil, e.nonce[:], data, nil)
+	return ciphertext, nil
 }
