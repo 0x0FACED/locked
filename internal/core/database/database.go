9a0378982f0dde83fcef692857ae18ff4c766e5d
0x0FACED <https.genius@gmail.com>
2024-11-11 13:40:20 +0300

    chore: added methods signatures of Database iface

--- internal/core/database/database.go
+++ internal/core/database/database.go
@@ -2,9 +2,8 @@ package database
 
 import (
 	"context"
+	"errors"
 	"os"
-
-	"github.com/0x0FACED/locked/internal/core/models"
 )
 
 type helper interface {
@@ -21,7 +20,7 @@ type Database interface {
 	// ReadFull читает фулл содержимое файла
 	ReadFull()
 	// Write записывает секрет в конец файла
-	Write(ctx context.Context, secret models.SecretRecord)
+	Write(ctx context.Context, secret []byte) error
 	// Flush очищает файл полностью, но файл не удаляет
 	Flush()
 	// Delete удаляет определенный секрет
@@ -37,6 +36,46 @@ type fileDatabase struct {
 	currentFile *os.File
 	currPos     uint64
 	secretCnt   uint8
+}
+
+func NewFileDatabase() Database {
+	return &fileDatabase{}
+}
+
+func (d *fileDatabase) Open(ctx context.Context, filename string) (*os.File, error) {
+	panic("not implemented") // TODO: Implement
+}
+
+func (d *fileDatabase) Read() {
+	panic("not implemented") // TODO: Implement
+}
+
+func (f *fileDatabase) ReadFull() {
+	panic("not implemented") // TODO: Implement
+}
+
+func (d *fileDatabase) Write(ctx context.Context, secret []byte) error {
+	n, err := d.currentFile.Write(secret)
+	if err != nil {
+		return err
+	}
+	if n != len(secret) {
+		return errors.New("~ Written data size not equal to secret size")
+	}
+
+	return nil
+}
+
+func (d *fileDatabase) Flush() {
+	panic("not implemented") // TODO: Implement
+}
+
+func (d *fileDatabase) Delete() {
+	panic("not implemented") // TODO: Implement
+}
+
+func (d *fileDatabase) DeleteN() {
+	panic("not implemented") // TODO: Implement
 }
 
 func (d *fileDatabase) Offset() uint64 {
